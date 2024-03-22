356563c41990f22896d14a2e2f0c53eb035dd11d
Ular <raevular01@gmail.com>
2024-03-23 02:18:11 +0600

    configured static files

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView
@@ -11,3 +13,6 @@ urlpatterns = [
     path('', include('recipe.urls')),
     path('', include('user.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
